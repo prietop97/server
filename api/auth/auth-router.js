bf829ab2e18e3da1a62d3e781b0313973f601ca4
Hector-bit <hectormartinez.2978@gmail.com>
2019-11-18 12:55:22 -0800

    authentication setup

---
+++ api/auth/auth-router.js
@@ -0,0 +1,57 @@
+const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const Users = require('../users/user-model');
+
+router.post('/register', (req, res) => {
+    let user = req.body;
+    const hash = bcrypt.hashSync(user.password, 10);
+    user.password = hash;
+
+    Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+})
+
+router.post('/login', (req, res) => {
+    let { username, password } = req.body;
+  
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          // produce a token
+          const token = getJwtToken(user);
+  
+          //send the token to the client
+          res.status(200).json({
+            message: `Welcome ${user.username}!`,
+            token
+          });
+        } else {
+          res.status(401).json({ message: 'Invalid Credentials' });
+        }
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  });
+  
+  function getJwtToken(user) {
+    const payload = {
+      user
+    };
+  
+    const secret = process.env.JWT_SECRET || "is it secret";
+  
+    const options = {
+      expiresIn: '1d'
+    }
+  
+    return jwt.sign(payload, secret, options);
+  }
--- api/server.js
+++ api/server.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const authRouter = require('../api/auth/auth-model');
+
 const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+server.use('/auth', authRouter)
 
 server.get('/', (req, res) => {
     res.send('Server is working')
 })
+
+server.use(auth);
 
 module.exports = server;
