120c64a0bbdaced24c3254a2d97140149b3054db
dominique maack <dominiquemaack@gmail.com>
2019-11-18 14:34:18 -0700

    user-information model and router set up and working with dummy data

--- api/server.js
+++ api/server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const server = express();
 
+const infoRouter = require('../api/users/user-router');
+
+server.use(express.json());
+
+server.use('/info', infoRouter);
+
 server.get('/', (req, res) => {
     res.send('Server is working')
 })
