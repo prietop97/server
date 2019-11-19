b1da001e00e0c650cd39fad9790dc6922d02335d
dominique maack <dominiquemaack@gmail.com>
2019-11-19 15:44:44 -0700

    re-added .select query on user-model

--- data/seeds/01-users.js
+++ data/seeds/01-users.js
@@ -1,11 +1,12 @@
+
 const bcrypt = require('bcryptjs');
 const password = 'password';
 
 exports.seed = function(knex) {
   return knex('users').insert([
-    {id: 1, username: 'dominique', password: bcrypt.hashSync(password)},
-    {id: 2, username: 'hector', password: bcrypt.hashSync(password)},
-    {id: 3, username: 'tom', password: bcrypt.hashSync(password)}
+    {id: 1, fullname: 'dominique maack', username: 'dominique', password: bcrypt.hashSync(password)},
+    {id: 2, fullname: 'hector martinez', username: 'hector', password: bcrypt.hashSync(password)},
+    {id: 3, fullname: 'tom test', username: 'tom', password: bcrypt.hashSync(password)}
   ])
    
 };
